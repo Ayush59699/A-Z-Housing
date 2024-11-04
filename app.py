02cdcd4fea956d4713c9002e1d57b5413e049856
Ayush Singh <96381373+Ayush59699@users.noreply.github.com>
2024-11-04 23:42:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import or_
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 from models import db, Admin, Customer, Professional, Service, ServiceRequest
 from forms import LoginForm, RegisterForm, ServiceForm,ProfessionalSignupForm
@@ -36,16 +37,37 @@ def load_user(user_id):
         return None
     
 
-with app.app_context():
+with app.app_context(): # CREATED DEFAULT DATABASE ENTRIES + Default Admin
     db.create_all()
 
     admin_email = 'admin@gmail.com'
     admin_user = Admin.query.filter_by(email=admin_email).first()
-    
     if not admin_user:
         admin_user = Admin(name='admin', email=admin_email, password='123456')
         db.session.add(admin_user)
-        db.session.commit()
+
+    if Service.query.count()==0:
+        ds=[
+            Service(name='A.C. Servicing', price=100.0, description='A.C. servicing and maintenance.', time_required='2 hours'),
+            Service(name='Plumbing', price=50.0, description='General plumbing services.', time_required='1 hour')
+        ]
+        db.session.bulk_save_objects(ds)
+
+    if Customer.query.count() == 0:
+            dc = [
+                Customer(name='Ayush Singh', email='ayush0987singh@gmail.com', password='123456', address='Ghosiyana Lucknow', pincode=226029),
+                Customer(name='Rajan Singh', email='rajan@gmail.com', password='123456', address='VIT Chennai', pincode=654321)
+            ]
+            db.session.bulk_save_objects(dc)
+
+    if Professional.query.count() == 0:
+        dp = [
+            Professional(name='Prakash Shukla', email='prakash@gmail.com', password='123456', service_name='Plumbing', experience=5, document='plumbing_cert.pdf', address='789 Oak St', pincode=789012),
+            Professional(name='Lalit Singh', email='lalit@gmail.com', password='123456', service_name='Electrical', experience=3, document='electrical_cert.pdf', address='321 Pine St', pincode=210987)
+        ]
+        db.session.bulk_save_objects(dp)
+
+    db.session.commit()
 
 
 
@@ -129,19 +151,6 @@ def register_professional():
 
 
 
-@app.route('/admin/dashboard/search')
-@login_required
-def admin_search():
-    return render_template('admin_search.html')
-
-
-@app.route('/admin/dashboard/summary')
-@login_required
-def admin_summary():
-    return render_template('admin_summary.html')
-
-
-
 @app.route('/logout')
 @login_required
 def logout():
@@ -149,40 +158,215 @@ def logout():
     print('You have been logged out.', 'success')
     return redirect(url_for('login'))
 
+# -------------------------------------------------------------------------------
 
-@app.route('/admin/dashboard')
+@app.route('/admin/dashboard') #ADMIN DASHBOARD HOME
 @login_required
 def admin_dashboard():
+    if not isinstance(current_user, Admin):
+        return redirect(url_for('home')) 
     customers = Customer.query.all()
     professionals = Professional.query.all()
-    return render_template('admin_dashboard.html',admin=current_user, customers=customers, professionals=professionals)
+    services=Service.query.all()
+    return render_template('admin/admin_dashboard.html',admin=current_user, customers=customers, professionals=professionals, services=services)
 
+@app.route('/admin/dashboard/search', methods=["GET","POST"]) # ADMIN DASHBOARD SEARCH
+@login_required
+def admin_search():
+    if not isinstance(current_user, Admin):
+        return redirect(url_for('home'))
+    
+    return render_template("admin/admin_search.html")
 
+@app.route('/admin/dashboard/summary', methods=["GET","POST"]) # ADMIN DASHBOARD SEARCH
+@login_required
+def admin_summary():
+    if not isinstance(current_user, Admin):
+        return redirect(url_for('home'))
+    
+    return render_template("admin/admin_summary.html")
+ # -----------------------------------------------------------------------------
+# ------------------------------------------------------------------------------ 
 
-@app.route('/customer/dashboard')
+@app.route('/customer/dashboard') # CUSTOMER DASH HOME
 @login_required
 def customer_dashboard():
-    return render_template('customer_dashboard.html', customer=current_user)
+    if not isinstance(current_user, Customer):
+        return redirect(url_for('home'))
+
+    service_history = ServiceRequest.query.filter_by(customer_id=current_user.id).all()
+    return render_template('customer/customer_dashboard.html', customer=current_user, service_history=service_history)
+
+@app.route('/customer/dashboard/search') # CUSTOMER DASH search
+@login_required
+def customer_search():
+    if not isinstance(current_user, Customer):
+        return redirect(url_for('home'))
+    return render_template('customer/customer_search.html', customer=current_user)
+
+@app.route('/customer/dashboard/summary') # CUSTOMER DASH summary
+@login_required
+def customer_summary():
+    if not isinstance(current_user, Customer):
+        return redirect(url_for('home'))
+    return render_template('customer/customer_summary.html', customer=current_user)
 
 
 
-@app.route('/professional/dashboard')
+@app.route('/customer/dashboard/cleaning_services')
+@login_required
+def cleaning_services():
+    cs1=Service.query.filter(
+        or_(
+        Service.name.ilike('%Cleaning%'),
+        Service.description.ilike('%Cleaning%')
+        )
+    ).all()
+    print(cs1)
+    return render_template("customer/cleaning_services.html", services=cs1, customer=current_user)
+
+
+@app.route('/customer/dashboard/ac_services')
+@login_required
+def ac_services():
+    cs2=Service.query.filter(
+        or_(
+        Service.name.ilike('%A.c.%'),
+        Service.description.ilike('%A.c.%')
+        )
+    ).all()
+    print(cs2)
+    return render_template("customer/ac_services.html", services=cs2, customer=current_user)
+
+
+
+
+
+
+
+
+
+@app.route("/customer/dashboard/cleaning_services/book/<uuid:service_id>", methods=["GET", "POST"])
+def book_service(service_id):
+    service = Service.query.get(service_id)
+    if request.method == "POST":
+        customer_id = current_user.id  
+        date_of_request = request.form.get("date_of_request")
+        date_of_completion = request.form.get("date_of_completion")
+        remarks = request.form.get("remarks")
+        
+        new_request = ServiceRequest(
+            service_id=service.id,
+            customer_id=customer_id,
+            date_of_request=date_of_request,
+            date_of_completion=date_of_completion,
+            remarks=remarks
+        )
+        
+        db.session.add(new_request)
+        db.session.commit()
+        flash("Service booked successfully!", "success")
+        return redirect(url_for('customer_dashboard'))
+
+    return render_template("customer/book_service.html", service=service)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ----------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+
+@app.route('/professional/dashboard')  # PRO DASH HOME
 @login_required
 def professional_dashboard():
-    return render_template('professional_dashboard.html', professional=current_user)
+    if not isinstance(current_user, Professional):
+        return redirect(url_for('home'))
+    return render_template('professional/professional_dashboard.html', professional=current_user) 
 
+@app.route('/professional/dashboard/search') 
+@login_required
+def professional_search():
+    if not isinstance(current_user, Professional):
+        return redirect(url_for('home'))
+    return render_template('professional/professional_search.html', professional=current_user) 
 
-@app.route('/service/new', methods=['GET', 'POST'])
+@app.route('/professional/dashboard/summary') 
+@login_required
+def professional_summary():
+    if not isinstance(current_user, Professional):
+        return redirect(url_for('home'))
+    return render_template('professional/professional_summary.html', professional=current_user) 
+
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+
+@app.route('/admin/dashboard/add_service', methods=['GET', 'POST'])
 @login_required
 def add_service():
-    form = ServiceForm()
-    if form.validate_on_submit():
-        new_service = Service(name=form.name.data, price=form.price.data, description=form.description.data)
+    if request.method == "POST":
+        name = request.form.get("name")
+        price = request.form.get("price")
+        description = request.form.get("description")
+        time_required = request.form.get("time_required")
+
+        new_service = Service(name=name, price=price, description=description, time_required=time_required)
+
         db.session.add(new_service)
         db.session.commit()
-        flash('Service added successfully!')
+        print("SERVICE ADDED SUCCESSFULLY!", "success")
         return redirect(url_for('admin_dashboard'))
-    return render_template('add_service.html', form=form)
+
+    return render_template("admin/add_service.html")
+
+@app.route('/admin/dashboard/edit_service/<uuid:id>', methods=["GET", "POST"])
+@login_required
+def edit_service(id):
+    service = Service.query.get_or_404(id)
+
+    if request.method == "POST":
+        service.name = request.form.get("name")
+        service.price = request.form.get("price")
+        service.description = request.form.get("description")
+        service.time_required = request.form.get("time_required")
+
+        db.session.commit()
+        flash("Service updated successfully!", "success")
+        return redirect(url_for('admin_dashboard'))
+
+    return render_template("admin/edit_service.html", service=service)
+
+@app.route('/admin/dashboard/delete_service/<uuid:id>', methods=["POST"])
+@login_required
+def delete_service(id):
+    service = Service.query.get_or_404(id)
+    db.session.delete(service)
+    db.session.commit()
+    print("SERVICE DELETED SUCCESSFULLY!", "success")
+    return redirect(url_for('admin_dashboard'))
+
+# ---------------------------------------------------------------------
+# ---------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
 
 @app.route("/about")
 def about():
@@ -192,6 +376,9 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+
+
 
 
 if __name__=='__main__':
