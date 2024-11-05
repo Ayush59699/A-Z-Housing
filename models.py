8cfbd74e2231340dccaeecff61ccbb9841290ad8
Ayush Singh <96381373+Ayush59699@users.noreply.github.com>
2024-11-05 23:55:20 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
-import os, uuid
+import uuid
 from sqlalchemy.dialects.postgresql import UUID
 
 db = SQLAlchemy()
@@ -33,19 +33,24 @@ class Professional(UserMixin, db.Model):
     pincode = db.Column(db.Integer)
 
 
-
 class Service(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = db.Column(db.String(80))
     price = db.Column(db.Float)
     description = db.Column(db.String(200))
     time_required = db.Column(db.String(50))
 
 class ServiceRequest(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    service_id = db.Column(db.Integer, db.ForeignKey('service.id'))
-    customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'))
-    professional_id = db.Column(db.Integer, db.ForeignKey('professional.id'), nullable=True)
-    date_of_request = db.Column(db.String(50))
-    status = db.Column(db.String(20), default="requested")
-    remarks = db.Column(db.String(200))
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    service_id = db.Column(UUID(as_uuid=True), db.ForeignKey('service.id'), nullable=False)
+    customer_id = db.Column(UUID(as_uuid=True), db.ForeignKey('customer.id'), nullable=False)
+    professional_id = db.Column(UUID(as_uuid=True), db.ForeignKey('professional.id'), nullable=True)
+    date_of_request = db.Column(db.String(50), nullable=False)  
+    date_of_completion = db.Column(db.String(50), nullable=True)  
+    status = db.Column(db.String(20), default="requested")  # assigned/closed/ requested
+    remarks = db.Column(db.String(200), nullable=True)
+
+    service = db.relationship('Service', backref='service_requests')
+    customer = db.relationship('Customer', backref='service_requests')
+    professional = db.relationship('Professional', backref='service_requests')
+    
